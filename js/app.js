0dd520a4b30d83f05f2842a2b0fd54b8510623fe
Frank <frank.lora.93@gmail.com>
2018-09-15 00:27:30 +0200

    Unificando ramas de trabajo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
 'use strict'
+const cifrasNumero = 4,
+numeroGuardado = window.sessionStorage;
 
-console.log('funcionando');
+(numeroGuardado.getItem("numero")) ? (console.log(JSON.parse(numeroGuardado.getItem("numero")))) : "";
+
+
+document.querySelector('#guardarNumero').addEventListener('click', guardarNumero);
+
+function guardarNumero(){
+    const entrada = document.querySelector('#miNumero'),
+    entradaVal = entrada.value,
+    entradaSize = entrada.value.length;
+    
+
+    ((entradaSize!== 4) || (!/^([0-9])*$/.test(entradaVal))) ? (
+        alert('Por favor, debe introducir un número de' + cifrasNumero + 'cifras'),
+        entrada.focus()
+    ) : (
+        recorrerNumero(entradaSize, entradaVal)
+    )
+
+    function recorrerNumero(largoCadena, valorCadena){
+    
+        //for (let i = 0; i < largoCadena; i++){
+            if(Number(valorCadena[0]) === 0) {
+                console.log('Su número no puede empezar por 0');
+                entrada.focus();
+                return false;
+            } else{
+                numeroGuardado.setItem("numero", JSON.stringify(valorCadena));
+                console.log(JSON.parse(numeroGuardado.getItem("numero")));
+                //console.log(valorCadena[i]);
+            }
+            
+        //}
+        
+    }
+
+}

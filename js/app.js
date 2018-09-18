4f9dce12dbdcf21fe7a2a57a96c3efefb5dad764
Frank <frank.lora.93@gmail.com>
2018-09-18 12:49:01 +0200

    Creando la base del js y las funcionalidades

--- js/app.js
+++ js/app.js
@@ -1,62 +1,66 @@
-'use strict'
+"use strict";
 const cifrasNumero = 4,
-numeroGuardado = window.sessionStorage,
-numeroValidado = numeroGuardado.getItem("numero");
+  numeroGuardado = window.sessionStorage,
+  numeroValidado = numeroGuardado.getItem("numero");
 
-(numeroValidado) ? (console.log(JSON.parse(numeroValidado))) : "";
+numeroValidado ? console.log(JSON.parse(numeroValidado)) : "";
 
+document
+  .querySelector("#guardarNumero")
+  .addEventListener("click", guardarNumero);
 
-document.querySelector('#guardarNumero').addEventListener('click', guardarNumero);
+function guardarNumero() {
+  const numero = document.querySelector("#miNumero"),
+    valorNumero = numero.value,
+    tamanoNumero = numero.value.length;
 
-if(numeroValidado) {
-    document.querySelector('.contenedor__datos__numero').innerHTML = '';
-    document.querySelector('#contenido').innerHTML = `
-        <h4>Mi N&uacute;mero es: ${JSON.parse(numeroGuardado.getItem("numero"))}</h4>
-    `
+  tamanoNumero !== 4 || !/^([0-9])*$/.test(valorNumero)
+    ? (alert(
+        "Por favor, debe introducir un número de " + cifrasNumero + " cifras"
+      ),
+      numero.focus())
+    : Number(valorNumero[0]) === 0
+      ? (alert("Su número no puede empezar por 0"),
+        document.querySelector("#miNumero").focus())
+      : recorrerNumero(tamanoNumero, valorNumero);
+
 }
 
-function guardarNumero(){
-    const entrada = document.querySelector('#miNumero'),
-    entradaVal = entrada.value,
-    entradaSize = entrada.value.length;
-    
-    ((entradaSize !== 4) || (!/^([0-9])*$/.test(entradaVal))) ? (
-        alert('Por favor, debe introducir un número de' + cifrasNumero + 'cifras'),
-        entrada.focus()
-    ) : (
-        recorrerNumero(entradaSize, entradaVal)
-    )
+function recorrerNumero(tamanoNumero, valorNumero) {
 
-    function recorrerNumero(largoCadena, valorCadena){
-    
-        //for (let i = 0; i < largoCadena; i++){
-            if(Number(valorCadena[0]) === 0) {
-                alert('Su número no puede empezar por 0');
-                entrada.focus();
-                return false;
-            } else{
-                numeroGuardado.setItem("numero", JSON.stringify(valorCadena));
-                if(numeroGuardado.getItem("numero")) {
-                    document.querySelector('.contenedor__datos__numero').innerHTML = '';
-                    document.querySelector('#contenido').innerHTML = `
-                        <h4>Mi N&uacute;mero es: ${JSON.parse(numeroGuardado.getItem("numero"))}</h4>
-                    `
-                }
-                
-                for (let i = 0; i < largoCadena; i++){
-                    if(numeroGuardado.getItem("numero").indexOf(valorCadena[i]) === -1){
-                        alert();
-                    }else{
-                        console.log(valorCadena[i]);
-                    }
-                   
-                }    
-                        
-                //console.log(valorCadena[i]);
-            }
-            
-        //}
-        
+  for(let i = 0; i < tamanoNumero; i++) {
+    let contador = 0;
+    var digitosRepetidos = false;
+    for(let j = 0; j < tamanoNumero; j++){
+      if(valorNumero[j] === valorNumero[i]){
+        contador++;
+      }
+  
     }
 
+    if(contador >= 2){
+      digitosRepetidos = true;
+      alert('Error, su número contiene dígitos repetidos');
+      document.querySelector("#miNumero").focus();
+      break;
+    }
+
+  } 
+
+  confirmarNumero(digitosRepetidos, tamanoNumero, valorNumero)
+
+}
+
+function confirmarNumero(digitosRepetidos, tamanoNumero, valorNumero){
+  if(!digitosRepetidos){
+    numeroGuardado.setItem("numero", JSON.stringify(valorNumero));
+    if(numeroGuardado.getItem("numero")) { 
+      document.querySelector(".contenedor__datos__numero").innerHTML = "";
+      document.querySelector("#contenido").innerHTML = `
+          <h4>Mi N&uacute;mero es: ${JSON.parse(
+              numeroGuardado.getItem("numero")
+          )}</h4>
+      `;
+    };
+  }
 }
